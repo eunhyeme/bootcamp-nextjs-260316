40360f7fd9163f2e561ea71352b2a3e9cca46636
eunhy <gp79810@gmail.com>
2026-03-16 14:22:19 +0900

    8강. Next.js의 레이아웃과 페이지 컴포넌트는 기본적으로는 서버 컴포넌트이다. "use client" 로 클라이언트 컴포넌트로 바꾸자

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-
+"use client";
 export default function Home() {
   return (
     <div>
       <h1>Spring, nextjs연동 프론트엔드 실습 레포지토리입니다.</h1>
-      
+
     </div>  
   );
 }
