727226e77d26fb14bb45e397003ab76370d5cbcc
eunhy <gp79810@gmail.com>
2026-03-16 15:51:48 +0900

    13-1강. useEffect(페이지가 로딩될때 한번만 실행되는 함수)
    useState에 쓰일 객체의 타입을 알수 없어 컴파일러가 에러를 보내면
    1. map(post:{}) 에서 명시하거나
    2. =useState<{}[]> 에서 명시하거나
    3. 맨위에 Interface를 선언할 것

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,13 +1,39 @@
+'use client';
+import { useState, useEffect } from "react";
+
+//3. interface를 사용하여 타입을 정의
+interface Post {
+    id: number;
+    title: string;
+}
+
 export default function Home() {
-    const posts = [
-        { id: 1, title: "Post 1", content: "Content 1" },
-        { id: 2, title: "Post 2", content: "Content 2" },
-        { id: 3, title: "Post 3", content: "Content 3" },
-    ]
+
+    //2. useState에 타입을 정의
+    const [posts, setPosts] = useState<{id:number, title:string}[]>([]);
+    
+    useEffect(() => { 
+        //컴포넌트가 실행되자마자 실행되는 함수, 
+        // 그뒤로는 실행되지 않아서 두번째 인자로 빈 배열을 넣어줌
+        
+        fetch("http://localhost:8080/api/v1/posts")
+        //fetch코드는  spring 58강 참고
+        .then(response => response.json())
+        .then(data => { 
+            console.log(data)
+
+            //1&3 setPosts에 타입을 정의
+            setPosts(data as Post[]);
+        });
+    }, []); 
+
+
     return (
             <ul>
                 {posts.map((post) => (
-                    <li key={post.id} className="p-2">{post.title}</li>
+                    <li key={post.id} className="p-2">
+                        {post.title}
+                    </li>
                 ))}
             </ul>
     )
